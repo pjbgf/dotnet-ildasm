ef5a1519f308bd4f8119ddda8be347a339916e7b
Paulo Gomes <paulo.gomes.uk@gmail.com>
2017-05-28 15:47:34 +0100

    Fix if syntax

--- run-cross-checks.sh
+++ run-cross-checks.sh
@@ -5,7 +5,8 @@ ECHO Executing dotnet core ildasm against netstandard1.6 library...
 dotnet run --framework netcoreapp1.0 --project src/dotnet-ildasm/dotnet-ildasm.csproj src/dotnet-ildasm.Sample/bin/Release/netstandard1.6/dotnet-ildasm.Sample.dll -o netcore_netstandard16.il
    
 
-if [ ! type "$mono" > /dev/null; ] then
+if [ ! type "$mono" > /dev/null; ]
+then
    
     ECHO Executing dotnet core ildasm against net45 library...
     dotnet run --framework netcoreapp1.0 --project src\dotnet-ildasm\dotnet-ildasm.csproj src\dotnet-ildasm.Sample\bin\Release\net45\dotnet-ildasm.Sample.dll -o netcore_net45.il
