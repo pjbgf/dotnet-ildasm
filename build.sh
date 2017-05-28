570b75adcc83a343e88a3028c89abbe40ae1f944
Paulo Gomes <paulo.gomes.uk@gmail.com>
2017-05-28 16:59:24 +0100

    Remove explict Build configuration. Update README with linux build badge.

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -c Release -f netcoreapp1.0 $@
-dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -c Release -f netstandard1.5 $@
-dotnet build src/dotnet-ildasm.Sample/dotnet-ildasm.Sample.csproj -c Release -f netstandard1.6 $@
+dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f netcoreapp1.0 $@
+dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f netstandard1.5 $@
+dotnet build src/dotnet-ildasm.Sample/dotnet-ildasm.Sample.csproj -f netstandard1.6 $@
 
 
 if [[ ! -v RUN_MONO_TESTS ]]
@@ -13,7 +13,7 @@ then
 
 else
 
-    dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -c Release -f net45 $@
-    dotnet build src/dotnet-ildasm.Sample/dotnet-ildasm.Sample.csproj -c Release -f net45 $@
+    dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f net45 $@
+    dotnet build src/dotnet-ildasm.Sample/dotnet-ildasm.Sample.csproj -f net45 $@
 
 fi
