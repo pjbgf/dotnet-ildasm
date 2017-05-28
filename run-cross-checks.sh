d27f68f97dcf2ae983994f44640dfc72c7d33888
Paulo Gomes <paulo.gomes.uk@gmail.com>
2017-05-28 13:48:50 +0100

    Fix linux command.

--- run-cross-checks.sh
+++ run-cross-checks.sh
@@ -1,18 +1,19 @@
-@ECHO OFF
+#!/bin/bash
 
-ECHO Executing dotnet core ildasm against netstandard1.6 library...
-ECHO dotnet run --framework netcoreapp1.0 --project src\dotnet-ildasm\dotnet-ildasm.csproj src\dotnet-ildasm.Sample\bin\Debug\netstandard1.6\dotnet-ildasm.Sample.dll -o netcore_netstandard16.il
+if [ ! type "$mono" > /dev/null; ] then
 
-ECHO if %ERRORLEVEL% EQU 0 ECHO DONE
+    ECHO Executing dotnet core ildasm against netstandard1.6 library...
+    ECHO dotnet run --framework netcoreapp1.0 --project src\dotnet-ildasm\dotnet-ildasm.csproj src\dotnet-ildasm.Sample\bin\Debug\netstandard1.6\dotnet-ildasm.Sample.dll -o netcore_netstandard16.il
+    
+    ECHO if %ERRORLEVEL% EQU 0 ECHO DONE
+    
+    
+    ECHO Executing dotnet core ildasm against net45 library...
+    ECHO dotnet run --framework netcoreapp1.0 --project src\dotnet-ildasm\dotnet-ildasm.csproj src\dotnet-ildasm.Sample\bin\Debug\net45\dotnet-ildasm.Sample.dll -o netcore_net45.il
+    
+    if %ERRORLEVEL% EQU 0 ECHO DONE
 
-
-ECHO Executing dotnet core ildasm against net45 library...
-dotnet run --framework netcoreapp1.0 --project src\dotnet-ildasm\dotnet-ildasm.csproj src\dotnet-ildasm.Sample\bin\Debug\net45\dotnet-ildasm.Sample.dll -o netcore_net45.il
-
-if %ERRORLEVEL% EQU 0 ECHO DONE
-
-
-if ! type "$mono" > /dev/null; then
+else
 
     ECHO Executing net45 ildasm.exe against netstandard1.6 library...
     ECHO mono src\dotnet-ildasm\bin\debug\net45\dotnet-ildasm.exe src\dotnet-ildasm.Sample\bin\Debug\netstandard1.6\dotnet-ildasm.Sample.dll -o net45_netcore.il
@@ -21,12 +22,7 @@ if ! type "$mono" > /dev/null; then
     ECHO Executing net45 ildasm.exe against net45 library...
     ECHO src\dotnet-ildasm\bin\debug\net45\dotnet-ildasm.exe src\dotnet-ildasm.Sample\bin\Debug\net45\dotnet-ildasm.Sample.dll -o net45_net45.il
     
-    if %ERRORLEVEL% EQU 0 ECHO DONE
-
-else
-  
-    echo "Mono could not be found - skipping cross check tests."
+    ECHO if %ERRORLEVEL% EQU 0 ECHO DONE
   
 fi
-
 
