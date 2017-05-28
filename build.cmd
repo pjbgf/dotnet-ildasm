02ac59dd281aaa13b7999c58a85641830bd01837
Paulo Gomes <paulo.gomes.uk@gmail.com>
2017-05-28 15:03:25 +0100

    Removing test project from build script.

--- build.cmd
+++ build.cmd
@@ -1,3 +1,6 @@
-dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj %*
-dotnet build src/dotnet-ildasm.Sample/dotnet-ildasm.Sample.csproj %*
-dotnet build src/dotnet-ildasm.Tests/dotnet-ildasm.Tests.csproj %*
+dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f netcoreapp1.0 %*
+dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f netstandard1.5 %*
+dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f net45 %*
+
+dotnet build src/dotnet-ildasm.Sample/dotnet-ildasm.Sample.csproj -f net45 %*
+dotnet build src/dotnet-ildasm.Sample/dotnet-ildasm.Sample.csproj -f netstandard1.5 %*
