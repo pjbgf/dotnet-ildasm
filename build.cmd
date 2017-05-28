a53f2ce0f10fe44e5fed23101e98f77e6390b6ba
Paulo Gomes <paulo.gomes.uk@gmail.com>
2017-05-28 13:06:08 +0100

    Force Release builds for cross checks in CI.

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
-dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj
-dotnet build src/dotnet-ildasm.Sample/dotnet-ildasm.Sample.csproj
-dotnet build src/dotnet-ildasm.Tests/dotnet-ildasm.Tests.csproj
+dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj %*
+dotnet build src/dotnet-ildasm.Sample/dotnet-ildasm.Sample.csproj %*
+dotnet build src/dotnet-ildasm.Tests/dotnet-ildasm.Tests.csproj %*
