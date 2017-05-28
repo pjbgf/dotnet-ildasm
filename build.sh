49155c5422fb465e8244067ff68fe53aaf76bb73
Paulo Gomes <paulo.gomes.uk@gmail.com>
2017-05-28 15:35:20 +0100

    Remove windows syntax in linux bash.

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f netcoreapp1.0 %*
-dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f netstandard1.5 %*
-dotnet build src/dotnet-ildasm.Sample/dotnet-ildasm.Sample.csproj -f netstandard1.5 %*
+dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f netcoreapp1.0 $@
+dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f netstandard1.5 $@
+dotnet build src/dotnet-ildasm.Sample/dotnet-ildasm.Sample.csproj -f netstandard1.5 $@
 
 
 if [ ! type "$mono" > /dev/null; ] then
 
-    dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f net45 %*
-    dotnet build src/dotnet-ildasm.Sample/dotnet-ildasm.Sample.csproj -f net45 %*
+    dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f net45 $@
+    dotnet build src/dotnet-ildasm.Sample/dotnet-ildasm.Sample.csproj -f net45 $@
 
 fi
