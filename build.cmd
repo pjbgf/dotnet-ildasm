68b065aded8bad9afda6068364b6c26d151bfe11
Paulo Gomes <paulo.gomes.uk@gmail.com>
2017-05-28 16:13:07 +0100

    Removed inverted on if.

--- build.cmd
+++ build.cmd
@@ -3,4 +3,4 @@ dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f netstandard1.5 %*
 dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f net45 %*
 
 dotnet build src/dotnet-ildasm.Sample/dotnet-ildasm.Sample.csproj -f net45 %*
-dotnet build src/dotnet-ildasm.Sample/dotnet-ildasm.Sample.csproj -f netstandard1.5 %*
+dotnet build src/dotnet-ildasm.Sample/dotnet-ildasm.Sample.csproj -f netstandard1.6 %*
