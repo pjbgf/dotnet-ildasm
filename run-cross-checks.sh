bb1f46ec9cdad63cb88db1f0c022f69d41f65774
Paulo Gomes <paulo.gomes.uk@gmail.com>
2017-05-28 14:02:14 +0100

    Removed windows cmd syntax.

--- run-cross-checks.sh
+++ run-cross-checks.sh
@@ -11,11 +11,9 @@ if [ ! type "$mono" > /dev/null; ] then
     ECHO Executing dotnet core ildasm against net45 library...
     ECHO dotnet run --framework netcoreapp1.0 --project src\dotnet-ildasm\dotnet-ildasm.csproj src\dotnet-ildasm.Sample\bin\Debug\net45\dotnet-ildasm.Sample.dll -o netcore_net45.il
     
-    if %ERRORLEVEL% EQU 0 ECHO DONE
+    ECHO if %ERRORLEVEL% EQU 0 ECHO DONE
 
-fi
-
-if [ type "$mono" > /dev/null; ] then
+else
 
     ECHO Executing net45 ildasm.exe against netstandard1.6 library...
     ECHO mono src\dotnet-ildasm\bin\debug\net45\dotnet-ildasm.exe src\dotnet-ildasm.Sample\bin\Debug\netstandard1.6\dotnet-ildasm.Sample.dll -o net45_netcore.il
