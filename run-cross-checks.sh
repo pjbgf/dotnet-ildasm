a75e57d7d21bde3ea8f58e8df06237eb7eee360d
Paulo Gomes <paulo.gomes.uk@gmail.com>
2017-05-28 15:16:56 +0100

    Amend on CI Script to run commands from .sh files.

--- run-cross-checks.sh
+++ run-cross-checks.sh
@@ -3,26 +3,18 @@
 if [ ! type "$mono" > /dev/null; ] then
 
     ECHO Executing dotnet core ildasm against netstandard1.6 library...
-    ECHO dotnet run --framework netcoreapp1.0 --project src\dotnet-ildasm\dotnet-ildasm.csproj src\dotnet-ildasm.Sample\bin\Debug\netstandard1.6\dotnet-ildasm.Sample.dll -o netcore_netstandard16.il
+    dotnet run --framework netcoreapp1.0 --project src/dotnet-ildasm/dotnet-ildasm.csproj src/dotnet-ildasm.Sample/bin/Release/netstandard1.6/dotnet-ildasm.Sample.dll -o netcore_netstandard16.il
+   
     
-    ECHO if %ERRORLEVEL% EQU 0 ECHO DONE
+    #ECHO Executing dotnet core ildasm against net45 library...
+    #dotnet run --framework netcoreapp1.0 --project src\dotnet-ildasm\dotnet-ildasm.csproj src\dotnet-ildasm.Sample\bin\Debug\net45\dotnet-ildasm.Sample.dll -o netcore_net45.il
     
-    
-    ECHO Executing dotnet core ildasm against net45 library...
-    ECHO dotnet run --framework netcoreapp1.0 --project src\dotnet-ildasm\dotnet-ildasm.csproj src\dotnet-ildasm.Sample\bin\Debug\net45\dotnet-ildasm.Sample.dll -o netcore_net45.il
-    
-    ECHO if %ERRORLEVEL% EQU 0 ECHO DONE
-
 else
 
     ECHO Executing net45 ildasm.exe against netstandard1.6 library...
     ECHO mono src\dotnet-ildasm\bin\debug\net45\dotnet-ildasm.exe src\dotnet-ildasm.Sample\bin\Debug\netstandard1.6\dotnet-ildasm.Sample.dll -o net45_netcore.il
-    ECHO if %ERRORLEVEL% EQU 0 ECHO DONE
     
     ECHO Executing net45 ildasm.exe against net45 library...
     ECHO src\dotnet-ildasm\bin\debug\net45\dotnet-ildasm.exe src\dotnet-ildasm.Sample\bin\Debug\net45\dotnet-ildasm.Sample.dll -o net45_net45.il
     
-    ECHO if %ERRORLEVEL% EQU 0 ECHO DONE
-  
 fi
-
