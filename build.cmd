718db3f8872a43364a595f1ed7ad948414dfbf18
Paulo Gomes <paulo.gomes.uk@gmail.com>
2017-05-28 16:54:45 +0100

    Expliciting release build.

--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
-dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f netcoreapp1.0 %*
-dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f netstandard1.5 %*
-dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f net45 %*
+dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -c Release -f netcoreapp1.0 %*
+dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -c Release -f netstandard1.5 %*
+dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -c Release -f net45 %*
 
-dotnet build src/dotnet-ildasm.Sample/dotnet-ildasm.Sample.csproj -f net45 %*
-dotnet build src/dotnet-ildasm.Sample/dotnet-ildasm.Sample.csproj -f netstandard1.6 %*
+dotnet build src/dotnet-ildasm.Sample/dotnet-ildasm.Sample.csproj -c Release -f net45 %*
+dotnet build src/dotnet-ildasm.Sample/dotnet-ildasm.Sample.csproj -c Release -f netstandard1.6 %*
