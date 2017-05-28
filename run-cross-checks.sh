b342a15060cf3361281ddd83d48a6cbf37030428
Paulo Gomes <paulo.gomes.uk@gmail.com>
2017-05-28 13:36:53 +0100

    Force build to target netcore in linux.

--- run-cross-checks.sh
+++ run-cross-checks.sh
@@ -15,11 +15,11 @@ if %ERRORLEVEL% EQU 0 ECHO DONE
 if ! type "$mono" > /dev/null; then
 
     ECHO Executing net45 ildasm.exe against netstandard1.6 library...
-    mono src\dotnet-ildasm\bin\debug\net45\dotnet-ildasm.exe src\dotnet-ildasm.Sample\bin\Debug\netstandard1.6\dotnet-ildasm.Sample.dll -o net45_netcore.il
-    if %ERRORLEVEL% EQU 0 ECHO DONE
+    ECHO mono src\dotnet-ildasm\bin\debug\net45\dotnet-ildasm.exe src\dotnet-ildasm.Sample\bin\Debug\netstandard1.6\dotnet-ildasm.Sample.dll -o net45_netcore.il
+    ECHO if %ERRORLEVEL% EQU 0 ECHO DONE
     
     ECHO Executing net45 ildasm.exe against net45 library...
-    src\dotnet-ildasm\bin\debug\net45\dotnet-ildasm.exe src\dotnet-ildasm.Sample\bin\Debug\net45\dotnet-ildasm.Sample.dll -o net45_net45.il
+    ECHO src\dotnet-ildasm\bin\debug\net45\dotnet-ildasm.exe src\dotnet-ildasm.Sample\bin\Debug\net45\dotnet-ildasm.Sample.dll -o net45_net45.il
     
     if %ERRORLEVEL% EQU 0 ECHO DONE
 
