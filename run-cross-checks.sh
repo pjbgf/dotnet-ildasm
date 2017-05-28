e139df262efeb6d00d45206aecbda2264396ce93
Paulo Gomes <paulo.gomes.uk@gmail.com>
2017-05-28 16:35:03 +0100

    Correcting paths for linux.

--- run-cross-checks.sh
+++ run-cross-checks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-echo Executing dotnet core ildasm against netstandard1.6 library...
+echo "Executing dotnet core ildasm against netstandard1.6 library..."
 dotnet run --framework netcoreapp1.0 --project src/dotnet-ildasm/dotnet-ildasm.csproj src/dotnet-ildasm.Sample/bin/Release/netstandard1.6/dotnet-ildasm.Sample.dll -o netcore_netstandard16.il
    
 
@@ -12,13 +12,13 @@ then
 
 else
    
-    echo Executing dotnet core ildasm against net45 library...
-    dotnet run --framework netcoreapp1.0 --project src\dotnet-ildasm\dotnet-ildasm.csproj src\dotnet-ildasm.Sample\bin\Release\net45\dotnet-ildasm.Sample.dll -o netcore_net45.il
+    echo "Executing dotnet core ildasm against net45 library..."
+    dotnet run --framework netcoreapp1.0 --project src/dotnet-ildasm/dotnet-ildasm.csproj src/dotnet-ildasm.Sample/bin/Release/net45/dotnet-ildasm.Sample.dll -o netcore_net45.il
     
-    echo Executing net45 ildasm.exe against netstandard1.6 library...
-    mono src\dotnet-ildasm\bin\debug\net45\dotnet-ildasm.exe src\dotnet-ildasm.Sample\bin\Release\netstandard1.6\dotnet-ildasm.Sample.dll -o net45_netcore.il
+    echo "Executing net45 ildasm.exe against netstandard1.6 library..."
+    mono src/dotnet-ildasm/bin/Release/net45/dotnet-ildasm.exe src/dotnet-ildasm.Sample/bin/Release/netstandard1.6/dotnet-ildasm.Sample.dll -o net45_netcore.il
     
-    echo Executing net45 ildasm.exe against net45 library...
-    mono src\dotnet-ildasm\bin\debug\net45\dotnet-ildasm.exe src\dotnet-ildasm.Sample\bin\Release\net45\dotnet-ildasm.Sample.dll -o net45_net45.il
+    echo "Executing net45 ildasm.exe against net45 library..."
+    mono src/dotnet-ildasm/bin/Release/net45/dotnet-ildasm.exe src/dotnet-ildasm.Sample/bin/Release/net45/dotnet-ildasm.Sample.dll -o net45_net45.il
     
 fi
