f3dc8044c86058647cb57f155e22d775835d8e4e
Paulo Gomes <paulo.gomes.uk@gmail.com>
2017-05-28 16:25:10 +0100

    Fixing bash scripts.

--- build.sh
+++ build.sh
@@ -6,8 +6,12 @@ dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f netstandard1.5 $@
 dotnet build src/dotnet-ildasm.Sample/dotnet-ildasm.Sample.csproj -f netstandard1.6 $@
 
 
-if [[ -z "${RUN_MONO_TESTS}" ]];
+if [[ ! -v RUN_MONO_TESTS ]]
 then
+
+    echo "Not building projects for mono."
+
+else
 
     dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f net45 $@
     dotnet build src/dotnet-ildasm.Sample/dotnet-ildasm.Sample.csproj -f net45 $@
--- run-cross-checks.sh
+++ run-cross-checks.sh
@@ -5,8 +5,12 @@ echo Executing dotnet core ildasm against netstandard1.6 library...
 dotnet run --framework netcoreapp1.0 --project src/dotnet-ildasm/dotnet-ildasm.csproj src/dotnet-ildasm.Sample/bin/Release/netstandard1.6/dotnet-ildasm.Sample.dll -o netcore_netstandard16.il
    
 
-if [ -z "${RUN_MONO_TESTS}" ];
+if [[ ! -v RUN_MONO_TESTS ]]
 then
+
+    echo "Not running tests for mono."
+
+else
    
     echo Executing dotnet core ildasm against net45 library...
     dotnet run --framework netcoreapp1.0 --project src\dotnet-ildasm\dotnet-ildasm.csproj src\dotnet-ildasm.Sample\bin\Release\net45\dotnet-ildasm.Sample.dll -o netcore_net45.il
