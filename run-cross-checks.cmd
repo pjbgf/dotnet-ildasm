e7e7d059b4af8e2a812a6fc797833ace29c3e7ea
Paulo Gomes <paulo.gomes.uk@gmail.com>
2017-05-30 20:05:37 +0100

    Amending cross checks to account for file extension change after making sample project a console app.

--- run-cross-checks.cmd
+++ run-cross-checks.cmd
@@ -18,7 +18,7 @@ if %ERRORLEVEL% EQU 0 ECHO DONE
 
 
 ECHO Executing dotnet core ildasm against net45 library...
-dotnet run --framework netcoreapp1.0 --project src\dotnet-ildasm\dotnet-ildasm.csproj "src\dotnet-ildasm.Sample\bin\%TARGET_RELEASE_FOLDER%\net45\dotnet-ildasm.Sample.dll" -o netcore_net45.il
+dotnet run --framework netcoreapp1.0 --project src\dotnet-ildasm\dotnet-ildasm.csproj "src\dotnet-ildasm.Sample\bin\%TARGET_RELEASE_FOLDER%\net45\dotnet-ildasm.Sample.exe" -o netcore_net45.il
 
 if %ERRORLEVEL% EQU 0 ECHO DONE
 
@@ -30,7 +30,7 @@ if %ERRORLEVEL% EQU 0 ECHO DONE
 
 
 ECHO Executing net45 ildasm.exe against net45 library...
-"src\dotnet-ildasm\bin\%TARGET_RELEASE_FOLDER%\net45\dotnet-ildasm.exe" "src\dotnet-ildasm.Sample\bin\%TARGET_RELEASE_FOLDER%\net45\dotnet-ildasm.Sample.dll" -o net45_net45.il
+"src\dotnet-ildasm\bin\%TARGET_RELEASE_FOLDER%\net45\dotnet-ildasm.exe" "src\dotnet-ildasm.Sample\bin\%TARGET_RELEASE_FOLDER%\net45\dotnet-ildasm.Sample.exe" -o net45_net45.il
 
 if %ERRORLEVEL% EQU 0 ECHO DONE
 
