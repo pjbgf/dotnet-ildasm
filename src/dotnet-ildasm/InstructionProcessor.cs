a0d69ddd793bf51028ebcff7f7b3e78e99e44617
Paulo Gomes <paulo.gomes.uk@gmail.com>
2017-05-30 19:17:15 +0100

    Adding support to IL generation for FieldDefinitions. Resolves bug of generating invalid .il if that was used in the source assembly. +semver: fix

---
+++ src/dotnet-ildasm/InstructionProcessor.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Text;
+using DotNet.Ildasm;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+namespace DotNet.Ildasm
+{
+    public class InstructionProcessor
+    {
+        private readonly IOutputWriter _outputWriter;
+
+        public InstructionProcessor(IOutputWriter outputWriter)
+        {
+            _outputWriter = outputWriter;
+        }
+
+        public void WriteInstruction(Instruction instruction)
+        {
+            var exportedIL = GetInstructionIL(instruction);
+            _outputWriter.WriteLine(exportedIL);
+        }
+
+        private string GetInstructionIL(Instruction instruction)
+        {
+            var instructionWithouOperand = $"IL_{instruction.Offset:x4}: {instruction.OpCode.ToString()}";
+            var operand = GetOperandIL(instruction);
+
+            return $"{instructionWithouOperand}{operand}";
+        }
+
+        private string GetOperandIL(Instruction instruction)
+        {
+            if (instruction.Operand == null)
+                return String.Empty;
+
+            var builder = new StringBuilder();
+            builder.Append(' ');
+
+            if (instruction.OpCode.OperandType == OperandType.InlineString)
+            {
+                builder.Append('"');
+                builder.Append(instruction.Operand);
+                builder.Append('"');
+            }
+            else
+            {
+                switch (instruction.Operand)
+                {
+                    case MethodReference methodReference:
+                        builder.Append($"{methodReference.ReturnType.ToILType()} {methodReference.DeclaringType.ToILType()}::{methodReference.Name}{GetMethodCallParameters(methodReference)}");
+                        break;
+                    case FieldDefinition fieldDefinition:
+                        builder.Append($"{fieldDefinition.FieldType.ToILType()} {fieldDefinition.DeclaringType.ToILType()}::{fieldDefinition.Name}");
+                        break;
+                    default:
+                        builder.Append(instruction.Operand);
+                        break;
+                }
+            }
+
+            return builder.ToString();
+        }
+
+        private static string GetMethodCallParameters(MethodReference method)
+        {
+            var builder = new StringBuilder();
+            builder.Append("(");
+
+            if (method.HasParameters)
+            {
+                for (int i = 0; i < method.Parameters.Count; i++)
+                {
+                    if (i > 0)
+                        builder.Append(", ");
+
+                    var parameterDefinition = method.Parameters[i];
+                    builder.Append($"{parameterDefinition.ParameterType.ToILType()} ");
+                    builder.Append(parameterDefinition.Name);
+                }
+            }
+
+            builder.Append(")");
+            return builder.ToString();
+        }
+    }
+}
