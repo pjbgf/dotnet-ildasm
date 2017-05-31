4bf8c797f8f304cc7d1354c9d3bec23f5371be7f
Paulo Gomes <paulo.gomes.uk@gmail.com>
2017-05-31 20:41:45 +0100

    Implementing benchmarking for FileOutputWriters.

---
+++ src/dotnet-ildasm.Benchmarks/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Jobs;
+using BenchmarkDotNet.Running;
+using DotNet.Ildasm;
+using DotNet.Ildasm.Adapters;
+using Mono.Cecil;
+
+namespace dotnet_ildasm.Benchmarks
+{
+    [CoreJob][ClrJob]
+    public class StringBuilderVSOutPut
+    {
+
+        internal static readonly Lazy<AssemblyDefinition> SampleAssembly = new Lazy<AssemblyDefinition>(() =>
+            Mono.Cecil.AssemblyDefinition.ReadAssembly("c:\\git\\dotnet-ildasm\\src\\dotnet-ildasm.Sample\\bin\\Debug\\net45\\dotnet-ildasm.Sample.exe"));
+
+        private static MethodDefinition _methodDefinition;
+        private static readonly IndentationProvider IndentationProvider = new IndentationProvider();
+
+        static StringBuilderVSOutPut()
+        {
+            var type = SampleAssembly.Value.MainModule.Types.FirstOrDefault(x => x.Name == "PublicClass");
+            _methodDefinition = type.Methods.FirstOrDefault(x => x.Name == "UsingTryCatch");
+        }
+
+        [Benchmark]
+        public void FileBufferedOutputWriter()
+        {
+            using (var fileStreamOutputWriter = new FileStreamOutputWriter(IndentationProvider, "C:\\git\\dotnet-ildasm\\dotnet-ildasm.Sample.il"))
+            {
+                var processor = new MethodProcessor(fileStreamOutputWriter);
+                processor.WriteBody(_methodDefinition);
+            }
+        }
+
+        [Benchmark]
+        public void FileOutputWriter()
+        {
+            var fileOutputWriter = new FileOutputWriter(IndentationProvider, "C:\\git\\dotnet-ildasm\\dotnet-ildasm.Sample.il");
+            var processor = new MethodProcessor(fileOutputWriter);
+            processor.WriteBody(_methodDefinition);
+            fileOutputWriter.Flush();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<StringBuilderVSOutPut>();
+        }
+    }
+}
---
+++ src/dotnet-ildasm/Adapters/FileStreamOutputWriter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace DotNet.Ildasm.Adapters
+{
+    public sealed class FileStreamOutputWriter : IOutputWriter, IDisposable
+    {
+        FileStream stream;
+        private readonly StringBuilder _stringBuilder;
+        private readonly IndentationProvider _indentationProvider;
+        private readonly string _filePath;
+
+        public FileStreamOutputWriter(IndentationProvider indentationProvider, string filePath)
+        {
+            stream = File.Open(filePath, FileMode.OpenOrCreate);
+            var bufferedStream = new BufferedStream(stream);
+            _stringBuilder = new StringBuilder();
+
+            var memoryStream = new MemoryStream();
+         
+            _indentationProvider = indentationProvider;
+            this._filePath = filePath;
+        }
+
+        public void Dispose()
+        {
+            if (stream != null)
+                stream.Dispose();
+        }
+
+        public void Write(string value)
+        {
+            byte[] bytes = ASCIIEncoding.ASCII.GetBytes(value);
+            stream.Write(bytes, 0, bytes.Length);
+        }
+
+        public void WriteLine(string value = "")
+        {
+            byte[] bytes = ASCIIEncoding.ASCII.GetBytes($"{_indentationProvider.Apply(value)}{Environment.NewLine}");
+            stream.Write(bytes, 0, bytes.Length);
+        }
+    }
+}
