50ca0b871112a1f7bdc96a4e8e592c2c70345b73
Paulo Gomes <paulo.gomes.uk@gmail.com>
2017-06-03 21:20:07 +0100

    Removing unnecessary class.

--- src/dotnet-ildasm/AssemblyDecompiler.cs
+++ src/dotnet-ildasm/AssemblyDecompiler.cs
@@ -8,19 +8,20 @@ namespace DotNet.Ildasm
     public class AssemblyDecompiler : IAssemblyDecompiler
     {
         private readonly IOutputWriter _outputWriter;
-        private readonly AssemblyReferencesProcessor _assemblyReferencesProcessor;
         private readonly ModuleDirectivesProcessor _moduleDirectivesProcessor;
 
         public AssemblyDecompiler(string assemblyPath, IOutputWriter outputWriter)
         {
             _outputWriter = outputWriter;
             _moduleDirectivesProcessor = new ModuleDirectivesProcessor(assemblyPath, outputWriter);
-            _assemblyReferencesProcessor = new AssemblyReferencesProcessor(outputWriter);
         }
         
         public void WriteAssemblyExternalReferences(AssemblyDefinition assembly)
         {
-            _assemblyReferencesProcessor.WriteAssemblyReferences(assembly.MainModule.AssemblyReferences);
+            foreach (var reference in assembly.MainModule.AssemblyReferences)
+            {
+                reference.WriteIL(_outputWriter);
+            }
         }
 
         public void WriteAssemblySection(AssemblyDefinition assembly)
