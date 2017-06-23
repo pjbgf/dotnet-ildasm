c73d2938c701cb1fa4a3b8f45eb4a6d1d08ee750
Paulo Gomes <paulo.gomes.uk@gmail.com>
2017-06-23 23:20:57 +0100

    Remove ilasm as AppVeyor does not have that installed.

--- run-cross-checks.cmd
+++ run-cross-checks.cmd
@@ -35,11 +35,10 @@ ECHO Executing net45 ildasm.exe against net45 library...
 if %ERRORLEVEL% EQU 0 ECHO DONE
 
 
-
-ECHO "Reassembling .IL's onto Portable executable files again."
-ilasm netcore_netstandard16.il /dll /output:netcore_netstandard16.dll
-ilasm netcore_net45.il /exe /output:netcore_net45.exe
-ilasm net45_netcore.il /exe /output:net45_netcore.exe
-ilasm net45_net45.il /exe /output:net45_net45.exe
+REM ECHO "Reassembling .IL's onto Portable executable files again."
+REM ilasm netcore_netstandard16.il /dll /output:netcore_netstandard16.dll
+REM ilasm netcore_net45.il /exe /output:netcore_net45.exe
+REM ilasm net45_netcore.il /exe /output:net45_netcore.exe
+REM ilasm net45_net45.il /exe /output:net45_net45.exe
 
 ENDLOCAL
