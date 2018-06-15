bb71cf9bbbd8b8bf6e31f6bfd4c835661416f271
Paulo Gomes <paulo.gomes@gmail.com>
2018-06-15 21:58:06 +0100

    Break apart build scripts for each .Net core version.

--- build.cmd
+++ build.cmd
@@ -1,4 +1,5 @@
 dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f netcoreapp1.0 %*
+dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f netcoreapp2.0 %*
 dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f netstandard1.5 %*
 dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f net45 %*
 
--- build.sh
+++ build.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
-
-dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f netcoreapp1.0 $@
-dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f netcoreapp2.0 $@
 dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f netstandard1.5 $@
 dotnet build src/dotnet-ildasm.Sample/dotnet-ildasm.Sample.csproj -f netstandard1.6 $@
+
+
+if [[ ! -v BUILD_NETCORE_10 ]]
+then
+
+    dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f netcoreapp1.0 $@
+
+fi
+
+if [[ ! -v BUILD_NETCORE_20 ]]
+then
+
+    dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f netcoreapp2.0 $@
+
+fi
+
+if [[ ! -v BUILD_NETCORE_21 ]]
+then
+
+    dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f netcoreapp2.1 $@
+
+fi
 
 
 if [[ ! -v RUN_MONO_TESTS ]]
