746092a2fcde43ec75fe31299234c4d22e6ba54d
Paulo Gomes <Paulo.Gomes.uk@gmail.com>
2019-08-19 15:38:23 +0100

    Ensure netcoreapp 2.1 and 2.2 are supported.

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f netcoreapp2.2 $@
+dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f netcoreapp2.1 $@
 dotnet build src/dotnet-ildasm.Sample/dotnet-ildasm.Sample.csproj -f netstandard2.0 $@
--- run-cross-checks.sh
+++ run-cross-checks.sh
@@ -2,7 +2,7 @@
 
 echo "[dotnet core] Executing ildasm against netstandard2.0 library..."
 rm -f netcore_netstandard20.il
-dotnet run --project src/dotnet-ildasm/dotnet-ildasm.csproj src/dotnet-ildasm.Sample/bin/Release/netstandard2.0/dotnet-ildasm.Sample.dll -o netcore_netstandard20.il
+dotnet run --project src/dotnet-ildasm/dotnet-ildasm.csproj --framework netcoreapp2.2 src/dotnet-ildasm.Sample/bin/Release/netstandard2.0/dotnet-ildasm.Sample.dll -o netcore_netstandard20.il
 
 if [[ ! -v RUN_REASSEMBLE_TESTS ]]
 then
