8cb422e0c30325f316647a83c893606aba3edb3c
Paulo Gomes <paulo.gomes.uk@gmail.com>
2019-10-08 06:40:56 +0100

    Add support for netcore 3.0
    Signed-off-by: Paulo Gomes <paulo.gomes.uk@gmail.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f netcoreapp3.0 $@
 dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f netcoreapp2.2 $@
 dotnet build src/dotnet-ildasm/dotnet-ildasm.csproj -f netcoreapp2.1 $@
 dotnet build src/dotnet-ildasm.Sample/dotnet-ildasm.Sample.csproj -f netstandard2.0 $@
