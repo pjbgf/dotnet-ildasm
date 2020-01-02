f9f52548e785a5a3ee5a86082c365364dd6b51ca
Paulo Gomes <pjbgf@linux.com>
2020-01-02 19:45:43 +0000

    Fix IL generation for Generics

--- src/dotnet-ildasm/Infrastructure/InstructionExtensions.cs
+++ src/dotnet-ildasm/Infrastructure/InstructionExtensions.cs
@@ -34,10 +34,25 @@ namespace DotNet.Ildasm
                             break;
                         case MethodReference methodReference:
                             var instanceString = methodReference.HasThis ? "instance " : string.Empty;
-                            writer.Write(
-                                $"{instanceString}{methodReference.ReturnType.ToIL()} class {methodReference.DeclaringType.ToIL()}::{methodReference.Name}");
+                            if (methodReference.CallingConvention == MethodCallingConvention.Generic)
+                            {
+                                var genericInstance = instruction.Operand as GenericInstanceMethod;
+                                if (genericInstance != null)
+                                {
+                                    writer.Write(
+                                        $"{instanceString}{genericInstance.ReturnType.ToString()} class {genericInstance.DeclaringType.ToIL()}::{genericInstance.Name}");
 
-                            WriteMethodCallParameters(methodReference, writer);
+                                    WriteGenericMethodCallParameters(genericInstance, writer);
+                                }
+                            }
+                            else
+                            {
+                                writer.Write(
+                                    $"{instanceString}{methodReference.ReturnType.ToIL()} class {methodReference.DeclaringType.ToIL()}::{methodReference.Name}");
+
+                                WriteMethodCallParameters(methodReference, writer);
+                            }
+
                             break;
                         case FieldDefinition fieldDefinition:
                             if (fieldDefinition.FieldType.IsGenericInstance || fieldDefinition.FieldType.MetadataType == MetadataType.Class)
@@ -48,7 +63,15 @@ namespace DotNet.Ildasm
                         default:
                             var operandType = instruction.Operand.GetType();
                             if (operandType.IsClass)
-                                writer.Write("class ");
+                            {
+                                writer.Write($"class ");
+
+                                var genericInstance = instruction.Operand as GenericInstanceType;
+                                if (genericInstance != null)
+                                {
+                                    writer.Write($"[{genericInstance.Scope.Name}]");
+                                }
+                            }
 
                             writer.Write(instruction.Operand.ToString());
                             break;
@@ -57,19 +80,76 @@ namespace DotNet.Ildasm
             }
         }
 
+
+        private static void WriteGenericMethodCallParameters(GenericInstanceMethod method, IOutputWriter writer)
+        {
+            // call !!0 class [mscorlib]System.Threading.Interlocked::CompareExchange<class [mscorlib]System.EventHandler`1<object>> ([out] !!0&, !!0, !!0)
+            writer.Write("<");
+            var argI = 0;
+            foreach (var arg in method.GenericArguments)
+            {
+                if (argI > 0)
+                    writer.Write(", ");
+
+                var argType = arg.GetElementType();
+                if (argType.MetadataType == MetadataType.Class)
+                    writer.Write("class ");
+
+                writer.Write(argType.ToIL());
+
+                var genericArg = arg as GenericInstanceType;
+                if (genericArg != null)
+                {
+                    writer.Write("<");
+
+                    var subArgI = 0;
+                    foreach (var subArg in genericArg.GenericArguments)
+                    {
+                        if (subArgI > 0)
+                            writer.Write(", ");
+
+                        writer.Write(subArg.GetElementType().ToIL());
+                    }
+
+                    writer.Write(">");
+                }
+            }
+            writer.Write(">");
+            writer.Write("(");
+
+            for (int i = 0; i < method.Parameters.Count; i++)
+            {
+                if (i > 0)
+                    writer.Write(", ");
+
+                var parameter = method.Parameters[i];
+                if (parameter.IsOut)
+                    writer.Write("[out] ");
+
+                writer.Write($"{parameter.ParameterType.FullName}");
+            }
+
+            writer.Write(")");
+        }
+
         private static void WriteMethodCallParameters(MethodReference method, IOutputWriter writer)
         {
             writer.Write("(");
 
             if (method.HasParameters)
             {
+                var genericIndex = 0;
                 for (int i = 0; i < method.Parameters.Count; i++)
                 {
                     if (i > 0)
                         writer.Write(", ");
 
                     var parameterDefinition = method.Parameters[i];
-                    writer.Write($"{parameterDefinition.ParameterType.ToIL()}");
+
+                    if (method.ContainsGenericParameter && i % 2 != 0)
+                        writer.Write($"!{genericIndex++}");
+                    else
+                        writer.Write($"{parameterDefinition.ParameterType.ToIL()}");
                 }
             }
 
